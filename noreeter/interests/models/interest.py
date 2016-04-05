24f1e377d7d38eb0d78768636c2b7055ce41352a
goohooh <goo_oo@naver.com>
2016-04-05 22:50:31 +0900

    update Interest model: ManyToMany relationship with User model

--- noreeter/interests/models/interest.py
+++ noreeter/interests/models/interest.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -5,6 +6,11 @@ class Interest(models.Model):
     name = models.CharField(
         unique=True,
         max_length=20,
+    )
+
+    user_set = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="interest_set",
     )
 
     def __str__(self):
