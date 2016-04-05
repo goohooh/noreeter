de3f5396d388f3b48b9cf1b46a0c3d9082f5c9fd
goohooh <goo_oo@naver.com>
2016-04-05 17:09:40 +0900

    update LoginView: add post function for POST request
    login success => redirect home.html
    login failed => redirect login.html

--- noreeter/users/views/login.py
+++ noreeter/users/views/login.py
@@ -1,5 +1,26 @@
+from django.contrib.auth import login, authenticate
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 
 class LoginView(TemplateView):
     template_name = "users/login.html"
+
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            username=username,
+            password=password,
+        )
+
+        if user:
+            login(
+                request,
+                user,
+            )
+
+            return redirect(reverse("home"))
+        return redirect(reverse("login"))
