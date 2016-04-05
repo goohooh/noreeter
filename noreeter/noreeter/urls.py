f4d48ff157ae5950ac96d5581f1a43ca726ea67d
goohooh <goo_oo@naver.com>
2016-04-05 18:10:05 +0900

    init SignupView: init signup.html

--- noreeter/noreeter/urls.py
+++ noreeter/noreeter/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from .views import HomeView
-from users.views import LoginView, LogoutView
+from users.views import LoginView, LogoutView, SignupView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name="home"),
+
     url(r'^login/$', LoginView.as_view(), name="login"),
     url(r'^logout/$', LogoutView.as_view(), name="logout"),
+    url(r'^signup/$', SignupView.as_view(), name="signup"),
 ]
