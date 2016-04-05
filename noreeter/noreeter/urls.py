af7e3d8bb878043caaf1adab7b5ab70c111e2ac7
goohooh <goo_oo@naver.com>
2016-04-05 16:54:19 +0900

    init LoginView: init login.html

--- noreeter/noreeter/urls.py
+++ noreeter/noreeter/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from .views import HomeView
+from users.views import LoginView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name="home"),
+    url(r'^login/$', LoginView.as_view(), name="login"),
 ]
