db2d11e1c7d8d21a4a832a3feccb397206bdc78b
goohooh <goo_oo@naver.com>
2016-04-05 17:28:55 +0900

    init LogouView: add login status into header.html

--- noreeter/noreeter/urls.py
+++ noreeter/noreeter/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from .views import HomeView
-from users.views import LoginView
+from users.views import LoginView, LogoutView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name="home"),
     url(r'^login/$', LoginView.as_view(), name="login"),
+    url(r'^logout/$', LogoutView.as_view(), name="logout"),
 ]
