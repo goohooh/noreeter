bad872415728f75dd008821ed332d0b7462efdff
goohooh <goo_oo@naver.com>
2016-04-05 19:21:28 +0900

    init ProfileView: init profile.html, add signup link to header.html

--- noreeter/noreeter/urls.py
+++ noreeter/noreeter/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from .views import HomeView
-from users.views import LoginView, LogoutView, SignupView
+from users.views import LoginView, LogoutView, SignupView, ProfileView
 
 
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name="login"),
     url(r'^logout/$', LogoutView.as_view(), name="logout"),
     url(r'^signup/$', SignupView.as_view(), name="signup"),
+    url(r'^profile/$', ProfileView.as_view(), name="profile"),
 ]
