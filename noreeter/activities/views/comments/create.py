9a532c9330d89446a6ff4d42f986980586075fb2
goohooh <goo_oo@naver.com>
2016-04-07 18:52:13 +0900

    init ActivityCommentCreateView: update detail.html

---
+++ noreeter/activities/views/comments/create.py
@@ -0,0 +1,26 @@
+from django.views.generic import CreateView
+
+from activities.models import Activity, Comment
+
+
+class ActivityCommentCreateView(CreateView):
+    model = Comment
+    fields = [
+        'content',
+    ]
+
+    def form_valid(self, form):
+        activity = Activity.objects.get(
+           id=self.kwargs.get('pk')
+        )
+
+        form.instance.activity = activity
+        form.instance.user = self.request.user
+
+        return super(ActivityCommentCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        activity = Activity.objects.get(
+            id=self.kwargs.get('pk')
+        )
+        return activity.get_absolute_url()
