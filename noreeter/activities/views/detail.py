3260fb8cc90245c18f4291374cf55cf8a4ef9a70
goohooh <goo_oo@naver.com>
2016-04-08 21:44:29 +0900

    update detail, create views: inherit LoginRequiredMixin

--- noreeter/activities/views/detail.py
+++ noreeter/activities/views/detail.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
 
 from activities.models import Activity
 
 
-class ActivityDetailView(DetailView):
+class ActivityDetailView(LoginRequiredMixin, DetailView):
     model = Activity
     template_name = "activities/detail.html"
