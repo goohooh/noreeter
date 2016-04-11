21ec476207259ae05920b05f6e315d4fffff965f
goohooh <goo_oo@naver.com>
2016-04-11 21:55:19 +0900

    update ActivityListView: display activity by user's town

--- noreeter/activities/views/list.py
+++ noreeter/activities/views/list.py
@@ -9,3 +9,8 @@ class ActivityListView(LoginRequiredMixin, ListView):
     template_name = "activities/list.html"
     context_object_name = "activities"
     ordering = '-updated_at'
+
+    def get_queryset(self):
+        user = self.request.user
+        query = Activity.objects.filter(town=user.town)
+        return query
