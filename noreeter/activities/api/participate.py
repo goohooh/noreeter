e19e0938c17f1187a0ebcd41d5492cf05a660aa8
goohooh <goo_oo@naver.com>
2016-04-12 22:40:48 +0900

    refactor ParticipateAPIView: replace HttpResponse as rest_framework's Response

--- noreeter/activities/api/participate.py
+++ noreeter/activities/api/participate.py
@@ -1,8 +1,5 @@
-import json
-
-from django.http import HttpResponse
-
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from activities.models import Activity
 
@@ -13,27 +10,18 @@ class ParticipateAPIView(APIView):
         activity = Activity.objects.get(id=self.kwargs["pk"])
 
         if request.user in activity.participant_set.all():
-            return HttpResponse(
-                json.dumps({
-                    "participation_state": True,
-                }),
-                content_type="application/json"
-            )
+            return Response({
+                "participation_state": True,
+            })
         if activity.is_full:
-            return HttpResponse(
-                json.dumps({
-                    "participation_state": False,
-                    "is_full": True,
-                }),
-                content_type="application/json"
-            )
-        return HttpResponse(
-            json.dumps({
+            return Response({
                 "participation_state": False,
-                "is_full": False,
-            }),
-            content_type="application/json"
-        )
+                "is_full": True,
+            })
+        return Response({
+            "participation_state": False,
+            "is_full": False,
+        })
 
     def post(self, request, *args, **kwargs):
         activity = Activity.objects.get(id=request.POST.get("activityID"))
